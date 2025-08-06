e08cbd2fc33566e18bd51759f4c41c52579bfc5c
naga <naga@ccit.com>
2025-08-06 07:50:04 +0530

    2 instances added

--- Cloudinfra.tf
+++ Cloudinfra.tf
@@ -2,14 +2,20 @@ provider "aws" {
   region = "ap-south-2"
 }
 
-resource "aws_iam_user" "ccitdev1user" {
+resource "aws_iam_user" "ccitdev1user"{
   name = "ccitdev1"
 }
 
-resource "aws_iam_user" "ccitdev2user" {
+resource "aws_iam_user" "ccitdev2user"{
   name = "ccitdev2"
 }
 
-resource "aws_iam_user" "ccitdev3user" {
+resource "aws_iam_user" "ccitdev3user"{
   name = "ccitdev3"
+}
+
+resource "aws_instance" "ccitinstance"{
+  ami="ami-06232d181481c0e00"
+  instance_type="t3.micro"
+  count =2
 }
