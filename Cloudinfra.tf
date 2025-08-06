f192168d340d763f301c9b86c176f41cae4f1c52
naga <naga@ccit.com>
2025-08-06 08:02:48 +0530

    sentinal policy appliedd

--- Cloudinfra.tf
+++ Cloudinfra.tf
@@ -1,21 +1,12 @@
 provider "aws" {
   region = "ap-south-2"
 }
-
-resource "aws_iam_user" "ccitdev1user"{
-  name = "ccitdev1"
-}
-
-resource "aws_iam_user" "ccitdev2user"{
-  name = "ccitdev2"
-}
-
-resource "aws_iam_user" "ccitdev3user"{
-  name = "ccitdev3"
-}
-
 resource "aws_instance" "ccitinstance"{
   ami="ami-06232d181481c0e00"
   instance_type="t3.micro"
-  count =2
+  
+  tags{
+    Name = "ccit instance"
+    Project = "ccit-dev"
+  }
 }
